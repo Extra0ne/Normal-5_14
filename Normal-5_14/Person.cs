3803107588102ef1bd38c44dfba1c4fbd03ad180
Vasya Bogatchuk <v380962437840@gmail.com>
2020-09-08 13:40:23 +0300

    no message

--- Normal-5_14/Person.cs
+++ Normal-5_14/Person.cs
@@ -32,39 +32,50 @@ namespace Normal_5_14
         public void output()
 
         {
-
-            if (experience < 1)
+            if (experience <1 ^ salary <1 ^ worked_hourse <1 )
             {
-                premium = 0.0;
+                Console.WriteLine("Вы ввели отрицательное число");
+                
             }
             else
             {
-                if (experience >= 1 && experience < 3)
+                if (experience < 1)
                 {
-                    premium = 0.05 * (salary * worked_hourse);
+                    premium = 0.0;
                 }
                 else
                 {
-                    if (experience >= 3 && experience < 5)
+                    if (experience >= 1 && experience < 3)
                     {
-                        premium = 0.08 * (salary * worked_hourse);
+                        premium = 0.05 * (salary * worked_hourse);
                     }
                     else
                     {
-                        if (experience >= 5)
+                        if (experience >= 3 && experience < 5)
                         {
-                            premium = 0.15 * (salary * worked_hourse); 
+                            premium = 0.08 * (salary * worked_hourse);
+                        }
+                        else
+                        {
+                            if (experience >= 5)
+                            {
+                                premium = 0.15 * (salary * worked_hourse);
+                            }
                         }
                     }
+                    
                 }
+                Console.WriteLine("\nИмя работника {0}", name);
+                Console.WriteLine("Стаж работы: {0}", experience);
+                Console.WriteLine("З/П в час: {0}", salary);
+                Console.WriteLine("Отработанно часов: {0}", worked_hourse);
+                Console.WriteLine("Зарплата: {0}", salary * worked_hourse);
+                Console.WriteLine("Премия: {0}", premium);
             }
 
-            Console.WriteLine("\nИмя работника {0}", name);
-            Console.WriteLine("Стаж работы: {0}", experience);
-            Console.WriteLine("З/П в час: {0}", salary);
-            Console.WriteLine("Отработанно часов: {0}", worked_hourse);
-            Console.WriteLine("Зарплата: {0}", salary * worked_hourse);
-            Console.WriteLine("Премия: {0}", premium);
+            
+
+            
         }
     }
 }
